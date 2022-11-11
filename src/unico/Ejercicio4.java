d514a52d69ae6226ebd226be9f4e27d1dde60c94
ricardofg2000 <ricardofg2000@gmail.com>
2022-11-11 07:59:44 +0100

    cambio en el enunciado

--- src/unico/Ejercicio4.java
+++ src/unico/Ejercicio4.java
@@ -7,7 +7,7 @@ public class Ejercicio4 {
 	public static void main(String[] args) {
 		/* Escribe un programa que calcule el salario semanal de 
 		 * un trabajador teniendo en cuenta que las horas ordinarias 
-		 * (40 primeras horas de trabajo) se pagan a 12 euros la hora. 
+		 * (40 primeras horas de trabajo) se pagan a 12 euros la hora
 		 * A partir de la hora 41, se pagan a 16 euros la hora.*/
 		
 		Scanner leer = new Scanner(System.in);
